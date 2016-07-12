2402c2fe2144dc982292327460a5fce2c78c9d50
NickHub4017 <nrv.rox.u@gmail.com>
2016-07-12 23:45:11 +0530

    Lect_1     Add first system call

--- myprog.c
+++ myprog.c
@@ -1,7 +1,9 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
-int main(void) {
-    printf(1, "Our first user program on xv6\n");
+int
+main(void)
+{
+    printf(1, "Unix V6 was released in the year %d\n", getyear());
     exit();
 }
